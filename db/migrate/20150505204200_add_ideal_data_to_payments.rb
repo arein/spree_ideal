ee62097617c9aedb71299a2664261018725d2cee
Alexander-Derek Rein <adr@ceseros.de>
2015-05-11 21:10:49 +0200

    Log added

--- db/migrate/20150505204200_add_ideal_data_to_payments.rb
+++ db/migrate/20150505204200_add_ideal_data_to_payments.rb
@@ -3,6 +3,7 @@ class AddIdealDataToPayments < ActiveRecord::Migration
   def change
     add_column :spree_payments, :ideal_hash, :string
     add_column :spree_payments, :ideal_transaction, :string
+    add_column :spree_payments, :ideal_log, :string
   end
 
 end
