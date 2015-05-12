4fe623663e8b4d8e5aae16408d7d3795c11377a6
Alexander-Derek Rein <adr@ceseros.de>
2015-05-12 09:51:27 +0200

    Default Credentials Added

--- install-test-project.sh
+++ install-test-project.sh
@@ -5,6 +5,6 @@ bundle exec rake test_app
 cd spec/dummy
 rake db:create RAILS_ENV=test
 rake db:migrate RAILS_ENV=test
-bin/rake db:seed RAILS_ENV=test
-#bin/rake spree_sample:load RAILS_ENV=test
+bin/rake db:seed RAILS_ENV=test ADMIN_EMAIL=spree@example.com ADMIN_PASSWORD=spree123
+bin/rake spree_sample:load RAILS_ENV=test
 cd ../..
