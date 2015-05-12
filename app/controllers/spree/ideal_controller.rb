b04a3aed49f2d397528c152bbec6b2f6d5f26910
Alexander-Derek Rein <adr@ceseros.de>
2015-05-12 16:05:28 +0200

    Bug Fixes

--- app/controllers/spree/ideal_controller.rb
+++ app/controllers/spree/ideal_controller.rb
@@ -1,6 +1,7 @@
 class Spree::IdealController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token, :only => :status
+  skip_before_filter :verify_authenticity_token, :only => [:status, :back]
+  skip_before_action :verify_authenticity_token, :only => :back
 
   def success
 
